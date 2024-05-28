7555cdd7e52cb806c1c2ec75de4ef527a114173d
Daniel Beachy <dbeachy20@georgefox.edu>
2024-05-28 16:56:15 -0700

    update homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import Header from "./Header";
+import Welcome from "./Welcome";
 
 const Homepage = () => {
   return (
     <Container fluid className="d-flex flex-column vh-100">
       <Row>
         <Header />
+        <Welcome />
       </Row>
       <Row className="flex-grow-1 bg-secondary"></Row>
     </Container>
