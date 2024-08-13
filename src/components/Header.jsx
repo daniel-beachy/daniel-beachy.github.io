bd4dba1e03df1f13b20603c3edb5e1deb0a2d340
Daniel Beachy <dbeachy20@georgefox.edu>
2024-08-12 19:58:06 -0700

    fix spacing for social links, open in new tab

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import linkedin from "../img/linkedin.png";
 const Header = () => {
   return (
     <Navbar expand="lg" className="navbar-dark bg-dark m-0">
-      <Container className="mx-4">
+      <Container fluid className="d-flex justify-content-between mx-4">
         <Navbar.Brand>Daniel Beachy</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -18,10 +18,18 @@ const Header = () => {
           </Nav>
         </Navbar.Collapse>
         <div className="social-icon d-flex">
-          <a href="https://www.linkedin.com/in/daniel-beachy/">
+          <a
+            href="https://www.linkedin.com/in/daniel-beachy/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img className="px-2" src={linkedin} alt="" height={25} />
           </a>
-          <a href="https://github.com/daniel-beachy">
+          <a
+            href="https://github.com/daniel-beachy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img className="px-2" src={github} alt="" height={25} />
           </a>
         </div>
