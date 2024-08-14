ba3d0a2628b7f00a4e6f966e3cd2d2f0f68d79f1
Daniel Beachy <dbeachy20@georgefox.edu>
2024-08-13 22:27:16 -0700

    adjust homepage

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,40 +1,54 @@
 import React from "react";
-import profile from "../img/profile.png";
+import profile from "../img/profile-dark-bg.png";
 import { Row, Col, Container } from "react-bootstrap";
 
 const Welcome = () => {
   return (
-    <Container>
-      <Row className="bg-secondary p-5">
-        <Col
-          sm={8}
-          className="text-light-blue d-flex flex-column justify-content-center align-items-center"
-        >
-          <h1 style={{ fontSize: "4rem" }}>Hello! I'm Daniel</h1>
-          <button
-            type="button"
-            className="btn btn-primary mt-2"
-            style={{ maxWidth: "200px" }}
-            onClick={() =>
-              window.open(
-                "https://github.com/daniel-beachy/daniel-beachy.github.io/blob/main/resume.png?raw=true",
-                "_blank"
-              )
-            }
-          >
-            See Resume
-          </button>
-        </Col>
-        <Col sm={4}>
+    <Container
+      fluid
+      className="d-flex align-items-center justify-content-center vh-100 bg-secondary text-light-blue"
+    >
+      <Row className="text-center">
+        <Col>
           <img
             src={profile}
+            alt=""
             style={{
-              maxHeight: "75vh",
-              objectFit: "cover",
+              maxHeight: "60vh",
+              objectFit: "contain",
               borderRadius: "25px",
               width: "100%",
+              padding: "25px",
             }}
           />
+          <h1 style={{ fontSize: "4rem" }}>Hello! I'm Daniel</h1>
+          <Row className="justify-content-center">
+            <button
+              type="button"
+              className="btn btn-primary mt-2 mx-4"
+              style={{ maxWidth: "200px" }}
+              onClick={() =>
+                window.open(
+                  "https://github.com/daniel-beachy/daniel-beachy.github.io/blob/main/resume.png?raw=true",
+                  "_blank"
+                )
+              }
+            >
+              See Resume
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary mt-2 mx-4"
+              style={{ maxWidth: "200px" }}
+              onClick={() =>
+                document
+                  .getElementById("projectsList")
+                  .scrollIntoView({ behavior: "smooth" })
+              }
+            >
+              View Projects
+            </button>
+          </Row>
         </Col>
       </Row>
     </Container>
